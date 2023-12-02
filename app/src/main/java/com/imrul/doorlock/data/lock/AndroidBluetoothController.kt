b4438ab2e55148dae37049c6ad253918bca07643
Imrul Anwar <imrulanwar836@gmail.com>
2023-12-02 13:21:36 +0600

    Controller & Receiver done

--- app/src/main/java/com/imrul/doorlock/data/lock/AndroidBluetoothController.kt
+++ app/src/main/java/com/imrul/doorlock/data/lock/AndroidBluetoothController.kt
@@ -3,6 +3,7 @@ package com.imrul.doorlock.data.lock
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothManager
 import android.content.Context
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import androidx.compose.animation.core.updateTransition
 import com.imrul.doorlock.Manifest
@@ -16,18 +17,7 @@ import kotlinx.coroutines.flow.update
 
 
 @SuppressLint("MissingPermission")
-class AndroidBluetoothController(private val context: Context):BluetoothController {
-
-    init {
-        updatePairedDevices()
-    }
-
-    private val bluetoothManager by lazy {
-        context.getSystemService(BluetoothManager::class.java)
-    }
-    private val bluetoothAdapter by lazy {
-        bluetoothManager?.adapter
-    }
+class AndroidBluetoothController(private val context: Context) : BluetoothController {
 
     private val _scannedDevices = MutableStateFlow<List<BluetoothDeviceDomain>>(emptyList())
     private val _pairedDevices = MutableStateFlow<List<BluetoothDeviceDomain>>(emptyList())
@@ -37,30 +27,57 @@ class AndroidBluetoothController(private val context: Context):BluetoothControll
     override val pairedDevices: StateFlow<List<BluetoothDevice>>
         get() = _pairedDevices.asStateFlow()
 
+    private val foundDeviceReceiver = FoundDeviceReceiver { device ->
+        _scannedDevices.update { devices ->
+            val newDevice = device.toBluetoothDeviceDomain()
+            if (newDevice in devices) devices else devices + newDevice
+        }
+
+    }
+
+    init {
+        updatePairedDevices()
+    }
+
+    private val bluetoothManager by lazy {
+        context.getSystemService(BluetoothManager::class.java)
+    }
+
+    private val bluetoothAdapter by lazy {
+        bluetoothManager?.adapter
+    }
+
     override fun startDiscovery() {
         if (!hasPermission((android.Manifest.permission.BLUETOOTH_SCAN))) {
             return
         }
+
+        context.registerReceiver(
+            foundDeviceReceiver,
+            IntentFilter(android.bluetooth.BluetoothDevice.ACTION_FOUND)
+        )
         updatePairedDevices()
         bluetoothAdapter?.startDiscovery()
     }
 
     override fun stopDiscovery() {
-        TODO("Not yet implemented")
+        if (!hasPermission(android.Manifest.permission.BLUETOOTH_SCAN)) {
+            return
+        }
+        bluetoothAdapter?.cancelDiscovery()
     }
 
     override fun release() {
-        TODO("Not yet implemented")
+        context.unregisterReceiver(foundDeviceReceiver)
     }
 
     private fun updatePairedDevices() {
-        if (!hasPermission(android.Manifest.permission.BLUETOOTH_CONNECT)){
+        if (!hasPermission(android.Manifest.permission.BLUETOOTH_CONNECT)) {
             return
         }
         bluetoothAdapter
             ?.bondedDevices
-            ?.map { it.toBluetoothDeviceDomain() }?.
-            also { devices ->
+            ?.map { it.toBluetoothDeviceDomain() }?.also { devices ->
                 _pairedDevices.update { devices }
             }
     }
